1228eca078cec1256ae4296e7d89e623f3827ed8
Chlorine <Chlorine1816@189.cn>
2021-02-20 10:10:16 +0800

    update py

--- code/TY_pan.py
+++ code/TY_pan.py
@@ -63,9 +63,9 @@ def main(arg1,arg2):
     response = s.get(surl,headers=headers)
     netdiskBonus = response.json()['netdiskBonus']
     if(response.json()['isSign'] == "false"):
-        sio.write(f"签到提示：未签到，获得{netdiskBonus}M\n\n")
+        sio.write(f"签到提示：未签到，获得{netdiskBonus}M 🎉\n\n")
     else:
-        sio.write(f"签到提示：已签到，获得{netdiskBonus}M\n\n")
+        sio.write(f"签到提示：已签到，获得{netdiskBonus}M 🎉\n\n")
     headers = {
         'User-Agent':'Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6',
         "Referer" : "https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1",
@@ -83,7 +83,7 @@ def main(arg1,arg2):
             sio.write("\n\n")
     else:
         description = response.json()['description']
-        sio.write(f"第一次抽奖：抽奖获得{description}\n\n")
+        sio.write(f"第一次抽奖：抽奖获得{description} 🎉\n\n")
     #第二次抽奖
     response = s.get(url2,headers=headers)
     if ("errorCode" in response.text):
@@ -95,7 +95,7 @@ def main(arg1,arg2):
             sio.write("\n\n")
     else:
         description = response.json()['description']
-        sio.write(f"第二次抽奖：抽奖获得{description}\n\n")
+        sio.write(f"第二次抽奖：抽奖获得{description} 🎉\n\n")
     desp = sio.getvalue()
     pushWechat(desp)
     return desp
